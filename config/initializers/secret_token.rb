14455af998f0a604d0709787e818fd6e3ae68da2
Tas Gray <tas@axiomit.com.au>
2013-08-23 15:42:05 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '830b8fbd9ca11b796d08dbebf62c10a985de2d41cc995ebbfc471e408c0ca75dd7545da1f34d5f918e25ab960175e01bc0f2aec6101c1e0e0e557ecaac9441df'
